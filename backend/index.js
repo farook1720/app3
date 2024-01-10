9af7876975200d82337d099811f661aaa4c2f2f2
SYED FAROOK BASHA <syed170791@gmail.com>
2024-01-10 13:19:17 +0530

    backend created

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.listen(4000, () => {
+  // console.log("Server is running on port 4000");
+})
+
+app.get('/', (req, res) => {
+  res.json("haii")
+})
+app.get('/test', (req, res) => {
+  res.json({ name: "kajs", age: 645 })
+})
+app.post('/test', (req, res) => {
+  const { name, fields } = req.query
+  res.json({ name, fields })
+})
+app.post('/test2', (req, res) => {
+  const { name, age } = req.body
+  res.json({ name, age })
+})
